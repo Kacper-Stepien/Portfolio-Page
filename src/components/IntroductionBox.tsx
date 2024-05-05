79e64734b137dc7ccced3bac85518cdec60649b3
Kacper Stępień <kacper2007x48@gmail.com>
2024-05-05 23:12:30 +0200

    add typing animation to IntroductionBox

--- src/components/IntroductionBox.tsx
+++ src/components/IntroductionBox.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import { IoLogoJavascript } from "react-icons/io5";
+import { TypeAnimation } from "react-type-animation";
 
 const IntroductionBox = () => {
+  const [textAnimationFinished, setTextAnimationFinished] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTextAnimationFinished(true);
+    }, 4000);
+  }, []);
+
   return (
-    <div className="bg-dark/[.8] w-min p-4 rounded-md">
+    <div className="bg-dark/[.8]  p-4 rounded-md w-[40rem]">
       <h3 className="text-6xl text-secondary font-bold">Hello There</h3>
       <h2 className="text-7xl text-light font-bold">I'm Kacper</h2>
+
       <div className="flex items-center gap-4">
-        <IoLogoJavascript className="text-8xl text-primary" />
-        <h1 className="text-8xl text-primary font-bold">Developer</h1>
+        {textAnimationFinished && (
+          <IoLogoJavascript className="text-8xl text-primary transition-all" />
+        )}
+        <TypeAnimation
+          className="text-8xl text-primary font-bold"
+          sequence={["Backend", 1000, "Frontend", 1000, "Developer", 1000]}
+          wrapper="span"
+          speed={50}
+          repeat={0}
+        />
       </div>
     </div>
   );
